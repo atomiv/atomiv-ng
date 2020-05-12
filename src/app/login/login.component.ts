bb8f1ab6bb0f9a3f8150b82bb8911c9747594d67
Jelena Cupac <j.cupac@gmail.com>
2020-05-12 18:08:41 +0200

    #2 Login component - html and scss code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  // NEW LOGIN FORM
+  // isAuthenticated: boolean;
 
-  constructor() { }
+  form: FormGroup; // loginForm
+  private formSubmitAttempt: boolean;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder, // fb
+    private authService: AuthService,
+  ) { }
+
+  ngOnInit() {
+    this.form  =  this.formBuilder.group({
+      // 'email': new FormControl(null, [Validators.required, Validators.email]),
+      // 'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+  }
+
+  isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
+  }
+
+  // login()
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService.login(this.form.value);
+    }
+    this.formSubmitAttempt = true;
+  }
+
+
+}
+
+
+/*
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service'; // ../auth.service
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
   }
 
 }
+*/
